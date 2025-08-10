a5ccb81e1fde945f0990945de40d33ea813b7f56
Caleb Steinwandt <caleb.steinwandt@gmail.com>
2025-08-10 16:50:46 -0600

    add error handling on App component

--- author/src/App.jsx
+++ author/src/App.jsx
@@ -3,23 +3,30 @@ import Logout from './Logout'
 import Navbar from './Navbar'
 import { useEffect, useState } from 'react'
 import Login from './Login'
+import ErrorPage from './ErrorPage'
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-  useEffect(() => {
-    try {
-      fetch(`${import.meta.env.VITE_URL}/api/posts/`, {
-        headers: {
-          authorization: `bearer ${localStorage.getItem("jwt")}`
-        }
-      }).then(response => {
-        if (response.ok) { setIsAuthenticated(true) }
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  }, [])
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_URL}/api/posts/`, {
+      headers: {
+        authorization: `bearer ${localStorage.getItem("jwt")}`
+      }
+    }).then(response => {
+      if (!response.ok) {
+        throw new Error("Network err")
+      }
+      setLoading(false);
+      setIsAuthenticated(true);
+
+    })
+      .catch(e => setError(e))
+      .finally(() => setLoading(false))
+  }, [])
+  if (loading) { return <><h1>Loading...</h1></> }
   if (isAuthenticated) {
     return (
       <>
@@ -29,9 +36,11 @@ function App() {
         <Logout />
       </>
     )
-  } else {
+  } else if (!error) {
     return <Login />
   }
+  if (error) { return <ErrorPage /> }
+
 }
 
 export default App
