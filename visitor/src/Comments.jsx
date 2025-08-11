89f124a4ab108a6b7b19141f35f6b04439dead89
cabelstein12 <Caleb.Steinwandt@gmail.com>
2025-08-11 13:31:28 -0600

    clean up formatting. add cancel button on reply

--- visitor/src/Comments.jsx
+++ visitor/src/Comments.jsx
@@ -3,7 +3,14 @@ import CommentForm from "./CommentForm";
 
 const Comments = ({ comments, setComments }) => {
     const [commenting, setCommenting] = useState(false);
+    const [replying, setReplying] = useState(false);
+    const [comment, setComment] = useState(null)
     const [error, setError] = useState(null)
+    const handleReply = (comment) => {
+        setComment(comment)
+        setCommenting(false);
+        setReplying(true)
+    }
     return (
         <>
             <h4>Comments</h4>
@@ -14,46 +21,57 @@ const Comments = ({ comments, setComments }) => {
                             <span>{new Date(comment.date).toDateString()}</span>
                             <span> {comment.author_name}:</span>
                             <div>{comment.body}</div>
-                            <div>
-                                <Replies replies={comment.replies} />
-                            </div>
+                            <Replies replies={comment.replies} />
+                            <button onClick={() => handleReply(comment)}>Reply</button> 
                         </div>
                     })}
                 </div> : <div>No comments</div>
             }
             {!commenting ?
-                <button onClick={() => setCommenting(true)}>Add Comment</button> :
+                <button onClick={() => {setCommenting(true); setReplying(false)}}>Add Comment</button> :
                 <CommentForm setComments={setComments} comments={comments} setCommenting={setCommenting} />
+            }
+            {replying ? 
+                <ReplyForm comment={comment} setReplying={setReplying}/> : ""
             }
         </>
     )
 }
 
-const ReplyForm = ({ comment }) => {
+const ReplyForm = ({ comment, setReplying }) => {
     const [error, setError] = useState(null);
     const handleReply = async (formdata) => {
         const reply = formdata.get("replyBody");
+        const author = formdata.get("author")
         await fetch(`${import.meta.env.VITE_URL}/api/posts/comments/${comment.id}`, {
 
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
-                "authorization": `bearer ${localStorage.getItem("jwt")}`,
             },
             method: "post",
-            body: `body=${encodeURIComponent(reply)}&author=author`
+            body: `body=${encodeURIComponent(reply)}&author=${encodeURIComponent(author)}`
 
         })
             .then(response => response.json())
             .then(response => { if (response.status >= 400) { throw new Error }; })
             .catch(error => setError(error))
     }
+    console.log(comment)
     return (
         <>
             {error ? <div style={{ color: "red" }}>an error occurred</div> : ''}
             <form action={handleReply}>
-                <label htmlFor="replyBody">Reply:</label>
-                <input type="text" name="replyBody" id="replyBody" />
-                <button>Reply</button>
+            <div>Replying to: {comment.author_name}</div>
+                <span>
+                    <label htmlFor="author">Author:</label>
+                    <input type="text" name="author" id="author" />
+                </span>
+                <span>
+                    <label htmlFor="replyBody">Reply:</label>
+                    <input type="text" name="replyBody" id="replyBody" />
+                </span>
+                <button>Submit</button>
+                <button onClick={()=>setReplying(false)}>Cancel</button>
             </form>
         </>
     )
@@ -62,31 +80,14 @@ const ReplyForm = ({ comment }) => {
 const Replies = ({ replies }) => {
     const [error, setError] = useState(null)
     const [commentReplies, setCommentReplies] = useState(replies);
-    const deleteReply = async (id) => {
-        console.log(id)
-        if (confirm('Delete reply?')) {
-            await fetch(`${import.meta.env.VITE_URL}/api/posts/replies/${id}`,
-                {
-                    method: "delete",
-                    headers: { "authorization": `bearer ${localStorage.getItem("jwt")}` },
-                }
-            )
-                .then(response => {
-                    if (!response.ok) { throw new Error(`HTTP error. Status: ${response.status}`) }
-                })
-                .catch(error => setError(error))
-            setCommentReplies(commentReplies.filter(r => r.id !== id))
-
-        }
-    }
     if(commentReplies){
         return ( commentReplies.map(reply => {
-            return <div key={reply.id}>
+            return <span key={reply.id} className="reply">
                 {error ? <div style={{ color: "red" }}>an error occurred</div> : ''}
-                <span>{reply.author_name} </span><span>{new Date(reply.date).toDateString()}</span>
-                <p>{reply.body}</p>
-                <button onClick={() => deleteReply(reply.id)}>Delete</button>
-            </div>
+                <span>{new Date(reply.date).toDateString()}</span>
+                <span>{reply.author_name}: </span>
+                <span>{reply.body}</span>
+            </span>
         }))
     }
 }
